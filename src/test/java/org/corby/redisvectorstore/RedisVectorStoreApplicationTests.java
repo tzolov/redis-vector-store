8cbb6afa523d7fbbe1d5de71f788b087495b4a09
Corby <pcorby@vmware.com>
2024-04-11 13:11:47 -0500

    First commit

---
+++ src/test/java/org/corby/redisvectorstore/RedisVectorStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.corby.redisvectorstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisVectorStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
