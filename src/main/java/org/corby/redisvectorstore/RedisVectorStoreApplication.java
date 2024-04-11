0bed8907bf90d1542475f027eed43726e68de1ee
Christian Tzolov <ctzolov@vmware.com>
2024-04-11 22:45:26 +0200

    Fix configuration and  add compose.yml

--- src/main/java/org/corby/redisvectorstore/RedisVectorStoreApplication.java
+++ src/main/java/org/corby/redisvectorstore/RedisVectorStoreApplication.java
@@ -1,11 +1,7 @@
 package org.corby.redisvectorstore;
 
-import org.springframework.ai.embedding.EmbeddingClient;
-import org.springframework.ai.vectorstore.RedisVectorStore;
-import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisVectorStoreApplication {
@@ -13,14 +9,4 @@ public class RedisVectorStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(RedisVectorStoreApplication.class, args);
     }
-
-    @Bean
-    public VectorStore redisVectorStore(EmbeddingClient embeddingClient) {
-        RedisVectorStore.RedisVectorStoreConfig config = RedisVectorStore.RedisVectorStoreConfig.builder()
-                .withURI("redis://localhost:6379")
-                .build();
-
-        return new RedisVectorStore(config, embeddingClient);
-    }
-
 }
